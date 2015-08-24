88f1406fd04302014772420c743d8fc139031005
Nebelwerfergranate <acht-acht@mail.ru>
2015-08-24 18:13:50 +0300

    Создал меню

--- labitint 2/Program.cs
+++ labitint 2/Program.cs
@@ -16,7 +16,7 @@ namespace labitint_2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMenu());
         }
     }
 }
